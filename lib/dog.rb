cba39abf9aa87e95cb92fb9c719b20c87b5bc65c
Stella McEwen <mcewen.stella@gmail.com>
2019-06-20 19:29:36 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,43 @@
+class Dog 
+  
+  attr_accessor :name, :breed, :id
+  
+  def initialize(id: nil, name:, breed:)
+    @name = name 
+    @breed = breed 
+    @id = id
+  end
+  
+  def self.create_table
+    sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL 
+    DROP TABLE IF EXISTS dogs 
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def self.new_from_db
+    
+  end
+  
+  def save
+    if self.id 
+      self.update 
+    else 
+      sql = <<-SQL 
+      INSERT INTO dogs(name, breed) 
+      VALUES (?, ?) 
+      SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
+  end
+  
+  
+end
