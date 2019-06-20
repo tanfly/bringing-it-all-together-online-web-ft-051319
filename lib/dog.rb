0486f954254e3dbe6ae92f51101cd514817cbf57
Stella McEwen <mcewen.stella@gmail.com>
2019-06-20 20:08:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,7 +87,7 @@ class Dog
     WHERE name = ? 
     AND breed = ?
     SQL
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    dog = DB[:conn].execute(sql, name, breed)
     if !dog.empty?
       self.new(id: dog[0][0], name: dog[0][1], breed: dog[0][2])
     else 
