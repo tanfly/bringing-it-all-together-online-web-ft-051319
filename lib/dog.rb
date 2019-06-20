1e5fa43f11c0ed532068b997342b8ebd03ed5840
Stella McEwen <mcewen.stella@gmail.com>
2019-06-20 19:38:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,8 +63,8 @@ class Dog
     end.first 
   end 
   
-  def self.create(name:, breed:)
-    dog = Dog.new(name:, breed:)
+  def self.create(name: breed:)
+    dog = Dog.new(name: breed:)
     dog.save 
     dog
   end
