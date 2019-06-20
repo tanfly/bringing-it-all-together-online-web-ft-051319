ad8aa19beafb4b7a571a6df05aa803b84e20978c
Stella McEwen <mcewen.stella@gmail.com>
2019-06-20 19:30:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,4 +44,14 @@ class Dog
   end 
   
   
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs 
+      WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first 
+  end 
+  
 end
