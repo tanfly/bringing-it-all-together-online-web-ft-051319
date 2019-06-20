e08be4ae0a80b34caa4e53387a69b2a9c420c2a8
Stella McEwen <mcewen.stella@gmail.com>
2019-06-20 19:35:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,6 +26,14 @@ class Dog
     
   end
   
+  def update 
+    sql = <<-SQL
+      UPDATE dogs SET name = ?, breed = ? 
+      WHERE id = ? 
+    SQL
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end 
+  
   def save
     if self.id 
       self.update 
